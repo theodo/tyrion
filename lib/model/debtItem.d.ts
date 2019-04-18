7e09e6b7aadf4923cccde6633fab89a9e6d89b7e
Maxime Thoonsen <maximet@theodo.fr>
2019-04-18 16:46:06 +0200

    :fire: remove lib directory

--- lib/model/debtItem.d.ts
+++
@@ -1,7 +0,0 @@
-export default class DebtItem {
-    type: string;
-    category: string;
-    comment: string;
-    fileName: string;
-    constructor(type: string, category: string, comment: string, fileName: string);
-}
--- lib/services/templateRenderer.d.ts
+++
@@ -1,4 +0,0 @@
-import DebtHistory from "../model/debtHistory";
-export default class TemplateRenderer {
-    static renderHtmlGraph(debtHistory: DebtHistory): void;
-}
